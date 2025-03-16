35870169631e34028bc33230950e045dbd6867cd
denys-zavhorodnii <dzavhorodnii@kse.org.ua>
2025-03-16 13:19:17 +0200

    init

---
+++ src/modules/clients/map/index.ts
@@ -0,0 +1,12 @@
+// export const getMapItems(): Promise<unknown> {
+//     // send request to API
+//     // parse response
+//     // check types
+//     // return data
+//     const API_KEY = "ccca023a91bdea4f63e8d074b850b53a2c330"
+//     const headers = new Headers()
+//     headers.set('x-apikey', API_KEY)
+//     fetch('https://mapstorage-7e78.restdb.io/rest/mapitem', {
+//         headers
+//     })
+// }
