0af2abbe1cbd41582a74a667b16ba628ad1b2557
denys-zavhorodnii <dzavhorodnii@kse.org.ua>
2025-03-20 12:30:34 +0200

    add add item page

--- src/Pages/addItemPage/AddItemPage.tsx
+++ src/Pages/addItemPage/AddItemPage.tsx
@@ -1,5 +1,6 @@
 import {Button} from '../../Components/Button/Button';
 import styles from './style.css';
+import {NotificationElement} from '../../Components/NotificationElement/NotificationElement';
 
 export const AddItemPage: React.FC = () => {
     const onLoginSubmit: React.FormEventHandler = (e) => {
@@ -8,17 +9,29 @@ export const AddItemPage: React.FC = () => {
     };
 
     return <form onSubmit={onLoginSubmit} className={styles.form}>
-            <div className={styles.row}>
-                <input  className={styles.input} type="text" placeholder='login' />
+        <div className={styles.row}>
+            <input className={styles.input} type="text" placeholder='Title'/>
+        </div>
+
+        <div className={styles.row}>
+            <div className={styles.subrow}>
+                <input className={styles.input} type="text" placeholder='Lat'/>
+            </div>
+            <div className={styles.subrow}>
+                <input className={styles.input} type="text" placeholder='long'/>
             </div>
 
-            <div  className={styles.row}>
-                <input  className={styles.input} type="pasword"  placeholder='password'/>
-            </div>
+        </div>
 
-            <div className={styles.row}>
-                <Button layout='fillAll'>Add Item</Button>
-            </div>
+        <div className={styles.row}>
+            <textarea name="" className={styles.textarea} ></textarea>
+        </div>
 
-        </form>;
+        <div className={styles.row}>
+            <Button layout='fillAll'>Add Item</Button>
+        </div>
+
+        <NotificationElement message='Something went wrong' level='error' />
+
+    </form>;
 };
